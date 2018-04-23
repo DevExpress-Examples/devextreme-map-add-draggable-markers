c52af1ce1afbc319fc1532a258a07bf267dec49b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/MapProvider/DxSample/app.js
@@ -0,0 +1,12 @@
+﻿window.DxSample = window.DxSample || {};
+
+$(function () {
+    DevExpress.devices.current({
+        platform: 'generic'
+    });
+    DxSample.app = new DevExpress.framework.html.HtmlApplication({
+        namespace: DxSample,
+        defaultLayout: DxSample.config.defaultLayout
+     });
+    DxSample.app.router.register(":view/:id", { view: "Index", id: undefined });
+});
---
+++ JS/MapProvider/DxSample/views/index.js
@@ -0,0 +1,23 @@
+﻿DxSample.Index = function (params) {
+    var viewModel = {
+        lat: ko.observable(40.749825),
+        lng: ko.observable(-73.987963),
+
+        mapReadyAction: function (s) {
+            var map = s.component;
+            
+            map.addMarker({
+                location: [viewModel.lat(), viewModel.lng()],
+                tooltip: 'Location: ' + viewModel.lat() + "; " + viewModel.lng()
+            }).done(function (marker) {
+                marker.setDraggable(true);
+                google.maps.event.addListener(marker, 'dragend', function (e) {
+                    viewModel.lat(e.latLng.lat());
+                    viewModel.lng(e.latLng.lng());
+                });
+            });
+
+        }         
+    };
+    return viewModel;
+}
