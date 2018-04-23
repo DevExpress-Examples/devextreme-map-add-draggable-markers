2c091b8245fe5a4867ed0788f2fc7753dba48389
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/DxSample/index.js
@@ -0,0 +1,60 @@
+﻿window.DxSample = window.DxSample || {};
+
+$(function() {
+    // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
+    // DevExpress.devices.current({ platform: "generic" });
+    // To customize the Generic theme, use the DevExtreme Theme Builder (http://js.devexpress.com/ThemeBuilder)
+    // For details on how to use themes and the Theme Builder, refer to the http://js.devexpress.com/Documentation/Guide/#themes article
+
+    var isDeviceReady = false,
+        isViewShown = false;
+
+    function hideSplashScreen() {
+        if(isDeviceReady && isViewShown) {
+            navigator.splashscreen.hide();
+        }
+    }
+
+    $(document).on("deviceready", function() {
+        isDeviceReady = true;
+        hideSplashScreen();
+        $(document).on("backbutton", function() {
+            DevExpress.processHardwareBackButton();
+        });
+    });
+
+    function onViewShown() {
+        isViewShown = true;
+        hideSplashScreen();
+        DxSample.app.off("viewShown", onViewShown);
+    }
+
+    function onNavigatingBack(e) {
+        if(e.isHardwareButton && !DxSample.app.canBack()) {
+            e.cancel = true;
+            exitApp();
+        }
+    }
+
+    function exitApp() {
+        switch(DevExpress.devices.real().platform) {
+            case "android":
+                navigator.app.exitApp();
+                break;
+            case "win":
+                MSApp.terminateApp('');
+                break;
+        }
+    }
+
+    DxSample.app = new DevExpress.framework.html.HtmlApplication({
+        namespace: DxSample,
+        layoutSet: DevExpress.framework.html.layoutSets[DxSample.config.layoutSet],
+        navigation: DxSample.config.navigation,
+        commandMapping: DxSample.config.commandMapping
+    });
+    DxSample.app.router.register(":view/:id", { view: "home", id: undefined });
+    DxSample.app.on("viewShown", onViewShown);
+    DxSample.app.on("navigatingBack", onNavigatingBack);
+    DxSample.app.navigate();
+});
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# dxMap - How to add draggable markers
+
+
+<p>This example illustrates an approach described in the <a href="https://www.devexpress.com/Support/Center/p/KA18782">How to take advantage of map provider API features that were not implemented in the dxMap widget?</a> Knowledge Base article.</p><p><strong>See also:</strong><br />
+<a href="http://www.devexpress.com/Support/Center/Example/Details/E4734"><u>dxMap - How to add the current location marker</u></a></p>
+
+<br/>
+
+
