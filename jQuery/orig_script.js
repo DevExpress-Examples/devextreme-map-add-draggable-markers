499dee4a401ba64acce5a43e0320824bb7438b5e
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-11-19 15:25:53 +0200

    Update to 25.1.3+

---
+++ jQuery/orig_script.js
@@ -0,0 +1,36 @@
+$(function(){  
+    var lat=40.749825;
+    var lng=-73.987963;
+    $("#txtLat").dxTextBox({
+        value: lat
+    });
+    $("#txtLng").dxTextBox({
+        value: lng
+    });
+    $("#map").dxMap({
+        apiKey: {
+            google: 'YOUR_API_KEY',
+        },
+        providerConfig: {
+            mapId: 'YOUR_MAP_ID',
+        },
+        zoom: 20,
+        width: 1200,
+        height: 700,
+        onReady: function (s) {
+            const map = s.component;
+    
+            map.addMarker({
+                location: [lat, lng],
+                tooltip: 'Location: ' + lat + "; " + lng
+            }).done((marker) => {
+                marker.gmpDraggable = true;
+    
+                marker.addListener('dragend', ({ latLng }) => {
+                    $("#txtLat").dxTextBox("instance").option("value", latLng.lat());
+                    $("#txtLng").dxTextBox("instance").option("value", latLng.lng());
+                });
+            });
+        }
+    });
+});
