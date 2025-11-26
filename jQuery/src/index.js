ef32dc2a9aa3c24a911429d561711069d63738ab
Artem Kurchenko <artem.kurchenko@devexpress.com>
2025-11-26 21:37:34 +0100

    jQuery: Modernize syntax with draggable marker functionality

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,40 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  const lat = 40.749825;
+  const lng = -73.987963;
+
+  $('#txtLat').dxTextBox({
+    value: lat,
+  });
+
+  $('#txtLng').dxTextBox({
+    value: lng,
+  });
+
+  $('#map').dxMap({
+    apiKey: {
+      google: 'YOUR_API_KEY',
+    },
+    providerConfig: {
+      mapId: 'YOUR_MAP_ID',
+    },
+    zoom: 20,
+    width: 1200,
+    height: 700,
+    onReady(s) {
+      const map = s.component;
+
+      map.addMarker({
+        location: [lat, lng],
+        tooltip: `Location: ${lat}; ${lng}`,
+      }).done((marker) => {
+        // eslint-disable-next-line spellcheck/spell-checker
+        marker.gmpDraggable = true;
+
+        marker.addListener('dragend', ({ latLng }) => {
+          $('#txtLat').dxTextBox('instance').option('value', latLng.lat());
+          $('#txtLng').dxTextBox('instance').option('value', latLng.lng());
+        });
+      });
     },
   });
 });
